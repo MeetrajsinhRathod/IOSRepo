01a2c4b8c6a1fdf4a9bb1a44a820e67f0663196a
Meetraj <rathod.meetraj.d@gmail.com>
2023-02-08 18:17:20 +0530

    Done examples of properties, property wrapper, subscripts

--- MyPlayground.playground/Pages/advancePractice.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/advancePractice.xcplaygroundpage/Contents.swift
@@ -106,7 +106,7 @@ funcWithAutoClosure(closure: "this is from closure")
 struct Resolution {
     var width: Int = 1
     var height: Int = 1
-    
+
     mutating func pixels(){
         width = 2
     }
@@ -115,62 +115,49 @@ var r = Resolution(width: 3, height: 4)
 r.pixels()
 r.width
 
-
 //Properties
 // stored and computed properties
 struct Circle {
     var radius: Double
     var center: (Int,Int)
-    
+
+    static var r: Double = 2.0
+
     var area: Double {
         get{
             return 3.14 * radius * radius
         }
+        set{
+            radius = newValue
+        }
     }
 }
 var ball = Circle(radius: 10.0, center: (0, 0))
-ball.area
-
+ball.area = 30.0
+ball.radius
 enum num{
     case caseOne
     case caseTwo
-    static var c = 10
-    
-    var x: String {
+    static var staticNum = 10
+
+    var numGet: String {
         switch self{
         case .caseOne: return "caseOne"
         case .caseTwo: return "caseTwo"
         }
     }
 }
-num.caseTwo.x
+num.caseTwo.numGet
 
-//read only computed properties
-class film {
-   var head = ""
-   var duration = 0.0
-   var metaInfo: [String:String] {
-      return [
-         "head": self.head,
-         "duration":"\(self.duration)"
-      ]
-   }
-}
-
-var movie = film()
-movie.head = "Properties"
-movie.duration = 2.0
-
-print(movie.metaInfo["head"]!)
-print(movie.metaInfo["duration"]!)
-
-// property observer
+// property observer & type properties
 class Samplepgm {
+    class var msg: String {
+        return "this is the msg"
+    }
    var counter: Int = 0 {
-      willSet(newTotal){
-         print("Total Counter is: \(newTotal)")
+      willSet {
+         print("Total Counter is: \(newValue)")
       }
-      
       didSet {
          if counter > oldValue {
             print("Newly Added Counter \(counter - oldValue)")
@@ -182,11 +169,12 @@ class Samplepgm {
 let NewCounter = Samplepgm()
 NewCounter.counter = 100
 NewCounter.counter = 800
+print(Samplepgm.msg)
 
 // property wrapper
 @propertyWrapper struct Capitalized {
     var wrappedValue: String {
-        didSet { wrappedValue = wrappedValue.capitalized }
+        didSet { wrappedValue = wrappedValue.uppercased() }
     }
 
     init(wrappedValue: String) {
@@ -198,6 +186,94 @@ struct User {
     @Capitalized var firstName: String
     @Capitalized var lastName: String
 }
+var user = User(firstName: "meetraj", lastName: "rathod")
+print(user.firstName)
+user.firstName = "hehehe"
+print(user.firstName)
 
-var user = User(firstName: "Meetraj", lastName: "Rathod")
-user.firstName = "Meet"
+@propertyWrapper
+struct Currency {
+    var rupee: String = ""
+    var wrappedValue: String {
+        get {
+            return rupee
+        }
+        set {
+            rupee = "₹" + newValue
+        }
+    }
+    init (wrappedValue: String){
+        self.wrappedValue = wrappedValue
+    }
+}
+
+struct Bill {
+    @Currency var amount: String
+}
+
+var electricityBill = Bill(amount: "10")
+print(electricityBill.amount)
+
+enum ColorCode {
+    case rgb(red: Int, green: Int, blue: Int)
+    mutating func doSomething(){
+        self = .rgb(red: 20, green: 30, blue: 40)
+    }
+}
+var rgbColor = ColorCode.rgb(red: 100, green: 0, blue: 0)
+print("rgb color code is : \(rgbColor)")
+rgbColor.doSomething()
+print("rgb color code is : \(rgbColor)")
+
+
+@propertyWrapper
+struct Confidential {
+    var val: String
+    let password: String
+
+    init(wrappedValue value: String, password: String) {
+        self.val = value
+        self.password = password
+    }
+    var wrappedValue: String {
+        get {
+            return val
+        }
+        set {
+            if checkPassword() {
+                val = newValue
+            }
+            else {
+                print("wrong password")
+            }
+        }
+    }
+    func checkPassword() -> Bool {
+        let enteredPassword = "password"
+        return enteredPassword == password
+    }
+}
+
+struct SecureData {
+    @Confidential var secretValue: String
+}
+
+var data = SecureData(secretValue: Confidential(wrappedValue: "confidential", password: "password"))
+data.secretValue = "new secret data"
+print(data.secretValue)
+
+//subscript
+class DaysOfAWeek {
+    var days = ["Monday", "Tuesday", "Wednesday",
+      "Thursday", "Friday", "Saturday", "Sunday"]
+   subscript(index: Int) -> String {
+      get {
+         return days[index]
+      }
+      set(newValue) {
+         self.days[index] = newValue
+      }
+   }
+}
+var day = DaysOfAWeek()
+print(day[0])
