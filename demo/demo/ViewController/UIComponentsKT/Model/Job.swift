03e20dadd3171e14e540c4326513dc43254c87df
Meetraj <rathod.meetraj.d@gmail.com>
2023-06-16 16:40:52 +0530

    Refactor Code and folders

---
+++ demo/demo/ViewController/UIComponentsKT/Model/Job.swift
@@ -0,0 +1,15 @@
+//
+//  Jobs.swift
+//  ReverseKT
+//
+//  Created by Meetrajsinh Rathod on 06/04/23.
+//
+
+import UIKit
+
+struct Job {
+    let companyImage: UIImage
+    let companyName: String
+    let jobRole: String
+    let salary: String
+}
---
+++ demo/demo/ViewController/UIComponentsKT/TableViewCell/JobSectionCell.swift
@@ -0,0 +1,30 @@
+//
+//  SectionCell.swift
+//  ReverseKT
+//
+//  Created by Meetrajsinh Rathod on 05/04/23.
+//
+
+import UIKit
+
+class JobSectionCell: UITableViewCell {
+
+    // MARK: - IBOutlet
+    @IBOutlet weak var jobLabel: UILabel!
+    @IBOutlet weak var seeMoreBtn: UIButton!
+    
+    // MARK: - delegate variable
+    var delegate: DataPass?
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    // MARK: - IBAction
+    @IBAction func seeMore(_ sender: Any) {
+        delegate?.seeAllJobs()
+    }
+    
+    
+
+}
